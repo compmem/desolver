316a5861c27b5bcf838e0395ad8907d9db107784
Per B. Sederberg <psederberg@gmail.com>
2008-08-17 15:45:55 -0400

    RF: Minor cleanup

--- examples/detest.py
+++ examples/detest.py
@@ -12,32 +12,25 @@ import desolver
 
 class MySolve(desolver.DESolver):
     #some test data
-    #xData = numpy.array([5.357, 9.861, 5.457, 5.936, 6.161, 6.731])
-    #yData = numpy.array([0.376, 7.104, 0.489, 1.049, 1.327, 2.077])
-
     def error_func(self, indiv, *args):
         # inverse exponential with offset, y = a * exp(b/x) + c
-        #predicted = indiv[0] * numpy.exp(indiv[1] / self.xData) + indiv[2]
         predicted = indiv[0] * numpy.exp(indiv[1] / args[0]) + indiv[2]
 
         # sum of squared error
-        #error = predicted - self.yData
         error = predicted - args[1]
         return numpy.sum(error*error)
 
 
 if __name__ == '__main__':
 
-    # parameterRanges, populationSize, maxGenerations, deStrategy,
-    # diffScale, crossoverProb, cutoffEnergy,
-    # useClassRandomNumberMethods, polishTheBestTrials
-    tStart = time.time()
-
+    # set the data
     xData = numpy.array([5.357, 9.861, 5.457, 5.936, 6.161, 6.731])
     yData = numpy.array([0.376, 7.104, 0.489, 1.049, 1.327, 2.077])
 
+    # start timer
+    tStart = time.time()
 
-    #solver = desolver.DESolver([(-100,100)]*3, 30, 600,
+    # set up the solver
     solver = MySolve([(-100,100)]*3, 30, 600,
                      method = desolver.DE_BEST_1,
                      args=[xData,yData], scale=0.7, crossover_prob=0.6,
