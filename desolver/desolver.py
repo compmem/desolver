d1bd3a86d8365686a39ee52ece843ae360496542
Per B. Sederberg <psederberg@gmail.com>
2010-01-29 22:00:04 -0500

    NF: Added license/copyright info.

--- desolver/desolver.py
+++ desolver/desolver.py
@@ -1,3 +1,11 @@
+#emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See the COPYING file distributed along with the DESolver package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
 #
 #
